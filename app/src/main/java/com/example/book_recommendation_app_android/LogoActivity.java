f4c72ff85f0e0aaae66c8ff7b3b7c8e5c551cab9
jaswanthveerepally <75801602+jaswanthveerepally@users.noreply.github.com>
2022-03-24 14:34:23 -0400

    added xml files

--- app/src/main/java/com/example/book_recommendation_app_android/LogoActivity.java
+++ app/src/main/java/com/example/book_recommendation_app_android/LogoActivity.java
@@ -2,13 +2,40 @@ package com.example.book_recommendation_app_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogoActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logos);
+
+        button = (Button) findViewById(R.id.loginBtn);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginActivity();
+                Intent intent = new Intent(context, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+
     }
+
+
+    private void openLoginActivity() {
+    }
+
+
 }
